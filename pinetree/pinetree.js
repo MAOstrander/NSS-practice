6db5cf18e2d74f64b7308c83157bb0bf0d82f664
MAOstrander <mathew.ostrander@gmail.com>
2015-10-16 11:38:08 -0500

    pinetree complete

--- pinetree/pinetree.js
+++ pinetree/pinetree.js
@@ -13,12 +13,27 @@
   //        *******
   //       *********
 
-
-
 // Create your function here. Make sure it takes the height argument.
 
 var tree = function(height) {
+  // var width = (height * 2) - 1; //Width will always be this mathematically
+  var pinetree = [];
+  pinetree[height] = "Midpoint";
+
+  for (var j = 0; j < pinetree.length; j++) {
+    pinetree[j] = " ";
+  }
+
+  for (var i = 0; i < height; i++) {
+
+    pinetree[height-i] = "*";
+    pinetree[height+i] = "*";
+
+    var pineOutput = pinetree.join("");
+    console.log(pineOutput);
+  }
 
 };
 
-tree(7);
+tree(42);
+
