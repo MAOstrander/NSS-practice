42759fc65520ffbd2f2329ce80e9680936de3163
MAOstrander <mathew.ostrander@gmail.com>
2015-10-16 20:42:02 -0500

    updated to include kelvin

--- temperature/temperature.js
+++ temperature/temperature.js
@@ -1,87 +1,88 @@
-/*
-  ...........YOUR MISSION...........
+//New and improved temperature converter
 
-  Write a program that will convert a temperature from
-  fahrenheit to celsius, or from celsius to fahrenheit.
+var convertField = document.getElementsByName("toConvert").item(0);
+var convertedF = document.getElementsByName("fanswer").item(0);
+var convertedC = document.getElementsByName("canswer").item(0);
+var convertedK = document.getElementsByName("kanswer").item(0);
 
-  1. In the HTML, have one input field where someone can enter
-      in a temperature.
-  2. Create a radio button group where Celsius or Fahrenheit 
-      can be selected as the scale that the number should be 
-      converted to.
-  3. Create a block level element that will hold the text of the
-      converted temperature.
-  4. Create a button that, when clicked, displays the converted.
-  5. Create another button that, when clicked, clears any text
-      in the input field.
-  6. Add an event handler to the input field that checks if the 
-      user pressed the enter key, and if that happens, perform
-      the conversion.
-  7. If the temperature is greater than 90F/32C the color of 
-      the converted temperature should be red.
-  8. If the temperature is less than 32F/0C the color of 
-      the converted temperature should be blue.
-  9. For any other temperature, the color should be green.
-*/
-var output = document.getElementById("output");
+function colorCode (temp) {
+    if (temp > 90) {
+    convertedF.className = "hot";
+    convertedC.className = "hot";
+    convertedK.className = "hot";
+  } else if (temp < 32) {
+    convertedF.className = "cold";
+    convertedC.className = "cold";
+    convertedK.className = "cold";
+  } else {
+    convertedF.className = "normal";
+    convertedC.className = "normal";
+    convertedK.className = "normal";
+  }
+}
 
-function toCelsius (convertMe) {
+function isFahrenheit (convertMe) {
   var cels = (convertMe - 32) * 5 / 9;
+  var kel = cels + 273.15;
+  var fahr = convertMe;
 
-  if (cels > 32) {
-    output.className = "hot";
-  } else if (cels < 0) {
-    output.className = "cold";
-  } else {
-    output.className = "normal";
-  }
+  convertedF.value = fahr;
+  convertedC.value = cels;
+  convertedK.value = kel;
 
-  // console.log("Answer in Celsius:", cels);
-  output.innerHTML = "Answer in Celsius: " + cels;
+  colorCode(fahr);
 }
 
-function toFahrenheit (convertMe) {
+function isCelsius (convertMe) {
+  var kel = convertMe + 273.15;
   var fahr = (convertMe * 9 / 5) + 32;
+  var cels = convertMe;
 
-    if (fahr > 90) {
-    output.className = "hot";
-  } else if (fahr < 32) {
-    output.className = "cold";
-  } else {
-    output.className = "normal";
-  }
+  convertedF.value = fahr;
+  convertedC.value = cels;
+  convertedK.value = kel;
 
-  // console.log("Answer in Fahrenheit:", fahr);
-  output.innerHTML = "Answer in Fahrenheit: " + fahr;
+  colorCode(fahr);
 }
 
-// This function should determine which conversion should happen based on which radio button is selected.
+function isKelvin (convertMe) {
+  var cels = convertMe - 273.15;
+  var fahr = (convertMe * 9 / 5) - 459.67;
+  var kel = convertMe;
+
+  convertedF.value = fahr;
+  convertedC.value = cels;
+  convertedK.value = kel;
+
+  colorCode(fahr);
+}
+
+// This function should determine which conversion formulas to apply based on which unit radio button is selected.
 function determineConverter (clickEvent) {
-  var unit = document.getElementsByName("units");
-  var numberToConvert = tempField.value;
+  var units = document.getElementsByName("units");
+  var numberToConvert = convertField.value;
 
-  // for (var i = 0; i < unit.length; i++) {
-  //   console.log(unit.item(i).checked);
-
-    if (unit.item(0).checked) {
-      toCelsius(numberToConvert); 
-    } else if (unit.item(1).checked) {
-      toFahrenheit(numberToConvert);
-    } 
-  // }
+    if (units.item(0).checked) {
+      isFahrenheit(numberToConvert);
+    } else if (units.item(1).checked) {
+      isCelsius(numberToConvert); 
+    } else if (units.item(2).checked) {
+      isKelvin(numberToConvert);
+    } else {
+      alert("Please Select A Unit Type!");
+    }
 
 }
 
 // This function checks what key is being pressed in the input field
 function submitWithEnter (keyUpEvent) {  
   if (keyUpEvent.keyIdentifier === "Enter") {
-    determineConverter(tempField.value);
+    determineConverter(this.value);
   }
 }
 
 // Runs conversion when you hit enter
-var tempField = document.getElementsByName("temp").item(0);
-tempField.addEventListener("keyup", submitWithEnter);
+convertField.addEventListener("keyup", submitWithEnter);
 
 // Convert Button
 var convertButton = document.getElementById("convert")
@@ -90,7 +91,11 @@ convertButton.addEventListener("click", determineConverter);
 // Clear Button
 var clearButton = document.getElementById("clear")
 clearButton.addEventListener("click", function(){
-  document.getElementsByName("temp").item(0).value = "";
-  output.innerHTML = 0;
-  output.className = "";
+  document.getElementsByName("toConvert").item(0).value = "";
+  convertedF.value = "";
+  convertedC.value = "";
+  convertedK.value = "";
+  convertedF.className = "";
+  convertedC.className = "";
+  convertedK.className = "";
 });
